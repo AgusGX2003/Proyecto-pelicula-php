3a77042548e3f7dda6789261e2e2a72f92420fe7
Agustin Piuca <aguspiucaquevedo@gmail.com>
2024-04-26 23:37:33 -0300

    Home view

--- movie-site-php-frontend/src/js/carousel.js
+++ movie-site-php-frontend/src/js/carousel.js
@@ -1,18 +1,18 @@
-class Carousel {
-    constructor() {
-        this.carouselCont = document.querySelectorAll('.carouselInfo');
-        this.template = document.querySelector('carouselTemplate').textContent;
-        this.fragment = new DocumentFragment()
-    }
+// class Carousel {
+//     constructor() {
+//         this.carouselCont = document.querySelectorAll('.carouselInfo');
+//         this.template = document.querySelector('carouselTemplate').textContent;
+//         this.fragment = new DocumentFragment()
+//     }
 
-    showCarouselInfo() {
-        this.carouselCont.forEach((img) => {
-            this.clone = this.template.cloneNode(true);
-            this.fragment.appendChild(this.clone)
-            img.appendChild(this.fragment);
+//     showCarouselInfo() {
+//         this.carouselCont.forEach((img) => {
+//             this.clone = this.template.cloneNode(true);
+//             this.fragment.appendChild(this.clone)
+//             img.appendChild(this.fragment);
 
-        })
-    }
-}
+//         })
+//     }
+// }
 
-export default Carousel;
+// export default Carousel;
--- movie-site-php-frontend/src/js/checker.js
+++ movie-site-php-frontend/src/js/checker.js
@@ -1,36 +1,36 @@
-class Checker {
-    verifyInput() {
-        const inputValue = document.querySelector('.myInput');
+// class Checker {
+//     verifyInput() {
+//         const inputValue = document.querySelector('.myInput');
 
-        if (inputValue.value === "") {
-            this.getErrorContainer("You must fill in all of the fields")
-        }
-    }
+//         if (inputValue.value === "") {
+//             this.getErrorContainer("You must fill in all of the fields")
+//         }
+//     }
 
-    getErrorContainer(errorMsg) {
-        const fragment = new DocumentFragment();
-        const errorContainer = document.querySelector(".errorContainer");
-        const errorTemplate = document.querySelector(".errorTemplate").content;
+//     getErrorContainer(errorMsg) {
+//         const fragment = new DocumentFragment();
+//         const errorContainer = document.querySelector(".errorContainer");
+//         const errorTemplate = document.querySelector(".errorTemplate").content;
 
-        const clone = errorTemplate.cloneNode(true)
-        fragment.appendChild(clone);
+//         const clone = errorTemplate.cloneNode(true)
+//         fragment.appendChild(clone);
 
-        errorContainer.appendChild(fragment);
-        document.querySelector(".alertError").innerText = errorMsg;
-        this.displayError(errorContainer);
-    }
+//         errorContainer.appendChild(fragment);
+//         document.querySelector(".alertError").innerText = errorMsg;
+//         this.displayError(errorContainer);
+//     }
 
-    displayError(errorContainer) {
-        errorContainer.style.display = "block"
+//     displayError(errorContainer) {
+//         errorContainer.style.display = "block"
 
-        setTimeout(() => {
-            errorContainer.style.display = "none";
+//         setTimeout(() => {
+//             errorContainer.style.display = "none";
 
-            while(errorContainer.firstChild) {
-                errorContainer.firstChild.remove();
-            }
-        }, 1500)
-    }
-}
+//             while(errorContainer.firstChild) {
+//                 errorContainer.firstChild.remove();
+//             }
+//         }, 1500)
+//     }
+// }
 
-export default Checker;
+// export default Checker;
